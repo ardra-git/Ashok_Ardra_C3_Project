6ca3aec4bb5ec253cdbd993af0a110a518f78bc8
ardra-git <95682821+ardra-git@users.noreply.github.com>
2022-01-05 20:40:18 +0530

    Item price

--- Restaurant/src/main/java/Item.java
+++ Restaurant/src/main/java/Item.java
@@ -10,6 +10,9 @@ public class Item {
     public String getName() {
         return name;
     }
+    public String getPrice() {
+    return price;
+    }
     @Override
     public String toString(){
         return  name + ":"
